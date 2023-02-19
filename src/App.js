01f2d5c84212889d9554d3107545995cf18c925a
wenchijchang <heyyhey@hotmail.com>
2023-02-19 00:41:49 -0500

    modified scr folder, created components folder and jsx files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  function createPostForm(post) {
+    let tempPosts = [...posts, post];
+    setPosts(tempPosts);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table></table>
     </div>
   );
 }
